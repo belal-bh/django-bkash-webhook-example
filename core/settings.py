a92c684b37b2587f63be558ae7131eafca71cde9
belal-bh <bh.pro.pust@gmail.com>
2021-07-05 07:58:26 +0600

    wb_bkash app created

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
+    'wb_bkash',
 ]
 
 MIDDLEWARE = [
---
+++ wb_bkash/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WbBkashConfig(AppConfig):
+    name = 'wb_bkash'
